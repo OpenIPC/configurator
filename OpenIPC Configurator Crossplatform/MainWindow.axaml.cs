c2eb8bb54289ab453034f57d15cf3852d7014af0
keyldev <wudgexyz@gmail.com>
2024-03-14 00:07:01 +0300

    feat: button handlers (cs file)

--- OpenIPC Configurator Crossplatform/MainWindow.axaml.cs
+++ OpenIPC Configurator Crossplatform/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System.Threading;
 
 namespace OpenIPC_Configurator_Crossplatform
 {
@@ -7,6 +8,52 @@ namespace OpenIPC_Configurator_Crossplatform
         public MainWindow()
         {
             InitializeComponent();
+
+            bFetch.Click += BFetch_Click;
+            bRead.Click += BRead_Click;
+            bUpload.Click += BUpload_Click;
         }
+
+        private void BUpload_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BRead_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BFetch_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /*
+         btnFetch
+        Process()
+            .StartInfo.UseShellExecute = False
+            .StartInfo.FileName = "C:\OpenIPCConfigurator\get.bat"
+            .StartInfo.Arguments = String.Format("{0}", txtIP.Text)
+            .StartInfo.RedirectStandardOutput = True
+            .Start()
+
+         */
+
+        /*
+        btnSend
+        Process()
+            .StartInfo.UseShellExecute = False
+            .StartInfo.FileName = "C:\OpenIPCConfigurator\send.bat"
+            .StartInfo.Arguments = String.Format("{0}", txtIP.Text)
+            .StartInfo.RedirectStandardOutput = True
+            .Start()
+
+         */
+
+        /*
+         
+
+         */
     }
 }
