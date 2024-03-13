43b7c7fc8bf64a3b42b25851848b5499d21f8c4d
keyldev <wudgexyz@gmail.com>
2024-03-13 23:40:08 +0300

    feat: relocating files

--- OpenIPCConfigurator/get.bat
+++
@@ -1,2 +0,0 @@
-pscp -scp -unsafe -pw 12345 root@%1:/etc/majestic.yaml /OpenIPCConfigurator/
-pscp -scp -unsafe -pw 12345 root@%1:/etc/wfb.conf /OpenIPCConfigurator/
--- OpenIPCConfigurator/send.bat
+++
@@ -1,2 +0,0 @@
-pscp -scp -unsafe -pw 12345 /OpenIPCConfigurator/majestic.yaml root@%1:/etc/ 
-pscp -scp -unsafe -pw 12345 /OpenIPCConfigurator/wfb.conf root@%1:/etc/ 
