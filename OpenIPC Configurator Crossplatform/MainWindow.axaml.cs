05d957f8963bf5aa5a56bdcf9fb3328a32293fba
keyldev <wudgexyz@gmail.com>
2024-03-14 00:27:09 +0300

    feat: fetch button function

--- OpenIPC Configurator Crossplatform/MainWindow.axaml.cs
+++ OpenIPC Configurator Crossplatform/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 
 namespace OpenIPC_Configurator_Crossplatform
@@ -12,6 +15,18 @@ namespace OpenIPC_Configurator_Crossplatform
             bFetch.Click += BFetch_Click;
             bRead.Click += BRead_Click;
             bUpload.Click += BUpload_Click;
+            bSaveWfb.Click += BSaveWfb_Click;
+            bSaveCameraSettings.Click += BSaveCameraSettings_Click;
+        }
+
+        private void BSaveCameraSettings_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BSaveWfb_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         private void BUpload_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
@@ -26,7 +41,22 @@ namespace OpenIPC_Configurator_Crossplatform
 
         private void BFetch_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (!File.Exists( AppDomain.CurrentDomain.BaseDirectory + "\\get.bat"))
+            {
+                //File.Create(AppDomain.CurrentDomain.BaseDirectory + "\\get.bat");
+                using var sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\get.bat");
+                // what mean /OpenIPCConfigurator? Is Default Folder?
+                sw.WriteLine("pscp -scp -unsafe -pw 12345 root@%1:/etc/majestic.yaml /OpenIPCConfigurator/\r\npscp -scp -unsafe -pw 12345 root@%1:/etc/wfb.conf /OpenIPCConfigurator/");
+                sw.Close();
+            }
+
+            var process = new ProcessStartInfo();
+            process.UseShellExecute = false;
+            process.FileName = AppDomain.CurrentDomain.BaseDirectory + "\\get.bat";
+            process.Arguments = string.Format("{0}", tbIpAddress.Text); // feat: check for correct ip
+            process.RedirectStandardOutput = true;
+            Process.Start(process);
+
         }
 
         /*
