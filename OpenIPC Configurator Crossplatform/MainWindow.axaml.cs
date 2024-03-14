86de7bbf7caaa5c83aec4cc45c9193d84d288cc2
keyldev <wudgexyz@gmail.com>
2024-03-14 23:46:43 +0300

    wip: reading

--- OpenIPC Configurator Crossplatform/MainWindow.axaml.cs
+++ OpenIPC Configurator Crossplatform/MainWindow.axaml.cs
@@ -49,7 +49,45 @@ namespace OpenIPC_Configurator_Crossplatform
 
         private void BRead_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            using var wfbReader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\wfb.conf");
+            using var camReader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\majestic.yaml");
+
+            
+
+            /*
+             Dim WFBreader As New IO.StreamReader("C:\OpenIPCConfigurator\wfb.conf")
+        Dim Camreader As New IO.StreamReader("C:\OpenIPCConfigurator\majestic.yaml")
+        Dim WFBallLines = New List(Of String)
+        Dim CamallLines = New List(Of String)
+        Do While Not WFBreader.EndOfStream
+            WFBallLines.Add(WFBreader.ReadLine)
+        Loop
+        WFBreader.Close()
+        txtFrequency.Text = ReadLine(7, WFBallLines)
+        txtPower.Text = ReadLine(10, WFBallLines)
+        txtFreq24.Text = ReadLine(8, WFBallLines)
+        txtPower24.Text = ReadLine(9, WFBallLines)
+        txtMCS.Text = ReadLine(14, WFBallLines)
+        txtSTBC.Text = ReadLine(12, WFBallLines)
+        txtLDPC.Text = ReadLine(13, WFBallLines)
+        txtFECK.Text = ReadLine(20, WFBallLines)
+        txtFECN.Text = ReadLine(21, WFBallLines)
+
+        Do While Not Camreader.EndOfStream
+            CamallLines.Add(Camreader.ReadLine)
+        Loop
+        Camreader.Close()
+        txtResolution.Text = ReadLine(28, CamallLines)
+        txtFPS.Text = ReadLine(29, CamallLines)
+        txtEncode.Text = ReadLine(25, CamallLines)
+        txtBitrate.Text = ReadLine(24, CamallLines)
+        txtExposure.Text = ReadLine(61, CamallLines)
+        txtContrast.Text = ReadLine(8, CamallLines)
+        txtHue.Text = ReadLine(9, CamallLines)
+        txtSaturation.Text = ReadLine(10, CamallLines)
+        txtLuminance.Text = ReadLine(11, CamallLines)
+        txtSensor.Text = ReadLine(60, CamallLines)
+             */
         }
 
         private void BFetch_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
