2987cf4b65fa789864bafcfe962fd4d7e79f8b36
MarioFPVdev <MarioFPV@outlook.com>
2025-11-15 19:29:58 +0000

    Update txprofiles in air_manager that was corrupted

--- Configurator.Designer.vb
+++ Configurator.Designer.vb
@@ -2875,7 +2875,7 @@ Partial Class Configurator
         Label12.Name = "Label12"
         Label12.Size = New Size(41, 10)
         Label12.TabIndex = 51
-        Label12.Text = "05/11/2025"
+        Label12.Text = "15/11/2025"
         ' 
         ' Label13
         ' 
